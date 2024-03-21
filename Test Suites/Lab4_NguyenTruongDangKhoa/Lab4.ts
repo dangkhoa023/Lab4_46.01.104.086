42f1acfc2bfc2483d00398383c8218e6a44ec98d
Khoa <hutieubonvatmi@gmail.com>
2024-03-21 12:17:09 +0700

    update

---
+++ Test Suites/Lab4_NguyenTruongDangKhoa/Lab4.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lab4</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1952f746-006e-4bfd-9dd3-035dd66ff509</testSuiteGuid>
+   <testCaseLink>
+      <guid>29919510-1ac7-4fad-838f-b7008c468e5b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lab4/Lab4</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>9237d79c-b3c3-42bf-a0b7-d5c3d32f8267</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value>7-7</value>
+         </iterationEntity>
+         <testDataId>Data Files/Login/Lab4</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>9237d79c-b3c3-42bf-a0b7-d5c3d32f8267</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>5fda479c-5625-41d5-925e-e038cd549a17</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>9237d79c-b3c3-42bf-a0b7-d5c3d32f8267</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Encrypt</value>
+         <variableId>249f8d80-a954-45bd-b7f5-1945ae4d4026</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
